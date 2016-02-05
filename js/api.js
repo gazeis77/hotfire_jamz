b670397eb92ca5879dc4d1e7421b4f2a8add8064
George Zeis <george.zeis@gmail.com>
2016-02-04 20:21:35 -0500

    changed api.js dramticaly, and changed index to follow changes.

--- js/api.js
+++ js/api.js
@@ -1,20 +1,44 @@
-$(document).ready(function() {
-
-    window.setTimeout(function() {
-        $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=scoonieapu&api_key=e09f97a093db384bc98e3c0004987fe7&limit=2&format=json&callback=?", function(data) {
-
-            var html = ''; 
-            var counter = 1; 
-            $.each(data.recenttracks.track, function(i, item) {
-                if(counter == 1) {
-                    html += '<span><a href="' + item.url + '" target="_blank">' + item.name + '</a> - ' + item.artist['#text'] + '</span>';
-                } 
-                counter++ 
-            });
-            $('.listening-to h5').append(html); // print the information to the document - here I look for the h5 tag inside the div with a class of 'listening-to' and use the jQuery append method to insert the information we've stored in the html variable inside the h5 tag.
-
-        });
-
-    },5000); // close JSON call
-
-}); // close document ready function
+/////////////////////
+// START DOM READY //
+/////////////////////
+$(function() {
+ 
+    // Runs function on page load
+    parse_lastfm_data();
+ 
+    // Runs function every interval thereafter
+    window.setInterval(function() {
+        parse_lastfm_data();
+    }, 5000); // Interval in milliseconds
+ 
+});
+// END DOM READY //
+ 
+function parse_lastfm_data() {
+ 
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=scoonieapu&api_key=e09f97a093db384bc98e3c0004987fe7&limit=2&format=json", function(data) {
+ 
+        // Uncomment console.log to see all returnable data
+        console.log(data);
+ 
+        // Checks for successful return before additional parsing.
+        if (typeof data == "object") {
+ 
+            // This just grabs the most recent track, key 0
+            // If you want to return more recent tracks you will have to loop through data["recenttracks"]["track"]
+            var artist = data["recenttracks"]["track"][0]["artist"]["#text"],
+                album = data["recenttracks"]["track"][0]["album"]["#text"],
+                title = data["recenttracks"]["track"][0]["name"],
+                url = data["recenttracks"]["track"][0]["url"];
+ 
+                $("#recent_artist").text(artist);
+                $("#recent_album").text(album);
+                $("#recent_title").text(title);
+                $("#recent_url").attr("href", url);
+ 
+ 
+        }
+ 
+    });
+ 
+}
