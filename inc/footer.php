7d50c3ae82ea998647714d2bd65ad51307bf6759
George Zeis <george.zeis@gmail.com>
2016-02-14 13:45:43 -0500

    added some content to get better direction on styling

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,15 @@
 <!--Footer-->
 <footer>
-  <div>
-    <div class="panel panel-default">
-      <div class="row panel-body">
+  <div class="navbar navbar-inverse navbar-fixed-bottom">
+    <div class="container">
+    
         <div class="footer">
           <a href="https://www.facebook.com/zeisphoto"><i class="fa fa-facebook-official fa-2x"></i></a>
           <a href="https://www.instagram.com/zeisphoto"><i class="fa fa-instagram fa-2x"></i></a>
           <a href="https://twitter.com/zeisphoto"><i class="fa fa-twitter fa-2x"></i></a>
           <p>&copy; 2016 Hotfire Jamz created by George Zeis</p>
         </div>
-      </div>
-    </div>
+    </div> 
   </div>
 </footer>
      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
--- index.php
+++ index.php
@@ -5,35 +5,21 @@ include("inc/header.php")
 
   <div id="main-content">
     <div class="container">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et magna eu velit lobortis posuere sit amet sit amet lacus. Cras quis imperdiet eros. In hac habitasse platea dictumst. Etiam efficitur non libero vitae vulputate. Etiam a augue neque. Phasellus volutpat eros id quam pretium, nec pulvinar orci faucibus. Donec consectetur mollis pulvinar. Duis suscipit mauris eget ante ultrices, malesuada ultrices felis malesuada. Pellentesque fermentum dictum purus eu tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut fringilla nisl arcu, in facilisis turpis sagittis sit amet. Ut eu rhoncus justo, quis sodales eros. In commodo laoreet nunc. Nulla mollis sed lorem non interdum. Fusce eu posuere velit, placerat rhoncus tellus.
-      </p>
 
-      <p>Ut metus nulla, euismod quis commodo et, tempus quis arcu. Mauris quis tincidunt tortor. Curabitur lectus nibh, pulvinar vel varius id, auctor luctus risus. Vestibulum et euismod arcu. Ut posuere neque ipsum, a euismod orci sagittis sed. Nulla ut faucibus lectus, sit amet laoreet arcu. Suspendisse diam lorem, consequat tincidunt dictum eu, tincidunt nec dui. Donec rhoncus luctus tellus. Suspendisse lectus diam, tempus eu interdum id, gravida et ex. Etiam auctor tortor ut arcu sodales lobortis. Suspendisse urna justo, dignissim sed imperdiet eu, fringilla at augue. Etiam dignissim mattis feugiat. Aliquam scelerisque a leo quis iaculis. Donec erat turpis, varius eu ullamcorper sed, elementum vitae lorem.
-      </p>
-    </div>
-  </div>
+    <h1>Spiritual Double Album</h1>
+    	<div>
+	    	<img src="img/Flying-Lotus-Youre-Dead.jpg">
+	    	<img src="img/kendrick-album-cover.jpg">
+    	</div>
+    	<p class="lead">Hear me out when I talk bout this.  <a href="https://open.spotify.com/album/29luvT98TnqHjVDYSRbbrj">Flyo's You're dead</a> record and <a href="https://open.spotify.com/album/7ycBtnsMtyVbbwTfJwRjSP"> Kendricks To Pimp a Butterfly</a> are a spiritual double album. <a href="http://www.hotnewhiphop.com/flying-lotus-kendrick-lamar-took-all-my-beats-news.11729.html"> If we take a look at what Steven Ellison posted on twitter</a> its seems cut and dry. Kendrick was given some beats and one was used (Wesley's Theory) but the story is more intertwined than that.</p>
 
+    	<p>When I first heard about the concept for “You're Dead” I was totally on board.  The idea of an electronic producer making a hard (link)Bebop record sounded right up my alley.  When people step outside of them selves and stretch their comfort zone.  Although this isn't completely out of the Flying Louts comfort zone because there is some precedence being the grand-nephew of Alice and John Coltrane.  This allows you to call in favors of sorts, like explaining I want to make a hard bop album about after being dead and having Herby Hancock jump on board.  And when you mix in long time conspirator and bass wizard Thundercat ones imagination can start to run wild.  I also pre ordered the vinyl boxset after I saw the insane artwork that came along with it.</p>
 
-
-
-
+    	<p>The record itself is shorter than I wanted, clocking in around 35 minutes.  It also feature more lyrics than any other Flylo release before.  Kendricks feature was nothing short of fantastic while Snoops Dead Mans Tetris feature (and the whole song) really wrecked the whole flow of the album for me. Yet this is no small feat of a record. This is something only Steven Ellison could make, something the identity of Flying Louts could create. While I happily listened to this record over and over again in the car I felt like the project wasn't completely over.</p>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	</div>
+</div>
 <?php
 include("inc/footer.php")
 ?>
